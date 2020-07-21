7474e70c4a34ee5e8e0c60e2b9822ccc2b5d6e2e
xiehangyi <825460964@qq.com>
2020-07-21 16:23:52 +0800

    fix(初始化)

--- src/views/typescript/gen.js
+++ src/views/typescript/gen.js
@@ -1,16 +1,10 @@
-"use strict";
-exports.__esModule = true;
-exports.getName = void 0;
-function getName(user) {
-    var Student = /** @class */ (function () {
-        function Student(firstName, lastName) {
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.fullName = this.firstName + ' * ' + this.lastName;
-        }
-        return Student;
-    }());
-    var student = new Student(user.firstName, user.lastName);
-    return student.fullName;
+function buildName(firstName, lastName) {
+    if (firstName) {
+        return firstName + ' ' + lastName;
+    }
+    else {
+        return lastName;
+    }
 }
-exports.getName = getName;
+var tomcat = buildName('Tom', 'Cat');
+var tom = buildName(undefined, 'Tom');
--- src/views/typescript/gen.ts
+++ src/views/typescript/gen.ts
@@ -1,15 +1,1 @@
-export function getName (user) {
-  class Student {
-    fullName: string;
-    constructor (public firstName: string, public lastName: string) {
-      this.fullName = this.firstName + ' * ' + this.lastName
-    }
-  }
-  interface Person {
-    firstName: string;
-    lastName: string;
-  }
-  
-  let student = new Student(user.firstName, user.lastName)
-  return student.fullName
-}
+
